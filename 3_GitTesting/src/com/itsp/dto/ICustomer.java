2dc357f370dd3c87fb640724b26f5d3b3879b6b2
vivek <vivek@DESKTOP-86B2GQB>
2021-05-07 21:02:07 +0530

    first commit

--- 3_GitTesting/src/com/itsp/dto/ICustomer.java
+++ 3_GitTesting/src/com/itsp/dto/ICustomer.java
@@ -1,15 +1,15 @@
 package com.itsp.dto;
 
 public class ICustomer {
- private int ;
+ private int i;
  private String a;
  private String b;
-<<<<<<< HEAD
- int i ;
- private float y;
-=======
- private int c;
- private flaot i ;
  
->>>>>>> branch 'master' of https://github.com/VK9396/ITSP-SPRING.git
+ int i1 ;
+ private float y;
+ 
+ private int c;
+ private float t3;
+ 
+ 
 }
