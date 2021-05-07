a0d60e357ea221f32e80beb3ef7b470069d53c8f
vivek <vivek@DESKTOP-86B2GQB>
2021-05-07 16:02:54 +0530

    code modition Commit

---
+++ 3_GitTesting/src/com/itsp/bo/Date.java
@@ -0,0 +1,10 @@
+package com.itsp.bo;
+
+public class Date {
+	
+	public java.util.Date getDate(){
+		return new java.util.Date();
+	}
+	
+
+}
---
+++ 3_GitTesting/src/com/itsp/test/Test.java
@@ -0,0 +1,10 @@
+package com.itsp.test;
+
+import com.itsp.bo.Date;
+
+public class Test {
+ public static void main(String[] args) {
+	Date d = new Date();
+	System.out.println(d.getDate());
+}
+}
